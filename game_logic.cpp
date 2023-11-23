e9c25463d2bce2eef5eff51fb5ddbad2e128409c
Syed-pixel <sh08467@st.habib.edu.pk>
2023-11-23 13:14:11 +0500

    all pieces coordinates are in arrays and are working fine

--- game_logic.cpp
+++ game_logic.cpp
@@ -9,46 +9,48 @@
 
 bool Game::Update()
 {   
+    int blue[41][2] = {{920, 650},
+    {835, 650}, {775, 650}, {715, 650}, {660, 650}, {605, 650},
+    {550, 650}, {490, 650}, {435, 650}, {380, 650}, {295, 705},
+    {295, 580}, {295, 525}, {295, 465}, {295, 410}, {295, 355},
+    {295, 295}, {295, 235}, {295, 180}, {295, 125}, {295, 50},
+    {380, 50}, {440, 50}, {495, 50}, {550, 50}, {610, 50},
+    {665, 50}, {720, 50}, {780, 50}, {835, 50}, {920, 50},
+    {920, 135}, {920, 190}, {920, 245}, {920, 305}, {920, 360},
+    {920, 420}, {920, 480}, {920, 535}, {920, 595}, {920, 650}};
 
-    int bottom[10] = {805,745,685,625,565,505,445,385,325,265}; //coordinates for each property in bottom section
-    int right[10] = {550,495,440,380,325,270,215,155,100,40}; //
-    int left[10] = {};
-    int top[10] = {};
-    // int player1vect[40][2]={[805,620],[745,620],[685,620],[625,620],[565,620],[505,620],[445,620],[385,620],[325,620],[265,620],[265,560],[265,500],[265,440],[265,380],[265,320],[265,260],[265,200],[265,140],[265,80],[265,20],[325,20],[385,20],[445,20],[505,20],[565,20],[625,20],[685,20],[745,20],[805,20],[890,20],[890,80],[890,140],[890,200],[890,260],[890,320],[890,380],[890,440],[890,500],[890,560],[890,620]};
-    // int player2vect[40][2]={[835,620],[775,620],[715,620],[655,620],[595,620],[535,620],[475,620],[415,620],[355,620],[295,620],[295,560],[295,500],[295,440],[295,380],[295,320],[295,260],[295,200],[295,140],[295,80],[295,20],[355,20],[415,20],[475,20],[535,20],[595,20],[655,20],[715,20],[775,20],[835,20],[920,20],[920,80],[920,140],[920,200],[920,260],[920,320],[920,380],[920,440],[920,500],[920,560],[920,620]};
-    // int player3vect[80]={830,620,770,620,710,620,650,620,590,620,530,620,470,620,410,620,350,620,290,620,290,560,290,500,290,440,290,380,290,320,290,260,290,200,290,140,290,80,290,20,350,20,410,20,470,20,530,20,590,20,650,20,710,20,770,20,830,20,890,20,890,80,890,140,890,200,890,260,890,320,890,380,890,440,890,500,890,560,890,620};
-    int player4vect[80]={830,620,770,620,710,620,650,620,590,620,530,620,470,620,410,620,350,620,290,620,290,560,290,500,290,440,290,380,290,320,290,260,290,200,290,140,290,80,290,20,350,20,410,20,470,20,530,20,590,20,650,20,710,20,770,20,830,20,890,20,890,80,890,140,890,200,890,260,890,320,890,380,890,440,890,500,890,560,890,620};
-    int player1vect[40][2] = {
-    {805,620},{745,620},{685,620},{625,620},{565,620},{505,620},{445,620},{385,620},{300,620},
-    {300,560},{300,500},{300,440},{300,300},{300,320},{300,260},{300,200},{300,140},{300,80},
-    {385,80},{445,80},{505,80},{565,80},{625,80},{685,80},{745,80},{805,80},{890,80},
-    {890,140},{890,200},{890,260},{890,320},{890,380},{890,440},{890,500},{890,560},{890,620}};
-//     int player1vect[40][2] = {
-//     {805,620},{745,620},{685,620},{625,620},{565,620},
-//     {505,620},{445,620},{385,620},{300,620},{265,620},
-//     {265,560},{265,500},{265,440},{265,380},{265,320},
-//     {265,260},{265,200},{265,140},{265,80},{265,20},
-//     {325,20},{385,20},{445,20},{505,20},{565,20},
-//     {625,20},{685,20},{745,20},{805,20},{890,20},
-//     {890,80},{890,140},{890,200},{890,260},{890,320},
-//     {890,380},{890,440},{890,500},{890,560},{890,620}
-// };
 
-int player2vect[40][2] = {
-    {835,620},{775,620},{715,620},{655,620},{595,620},
-    {535,620},{475,620},{415,620},{355,620},{295,620},
-    {295,560},{295,500},{295,440},{295,380},{295,320},
-    {295,260},{295,200},{295,140},{295,80},{295,20},
-    {355,20},{415,20},{475,20},{535,20},{595,20},
-    {655,20},{715,20},{775,20},{835,20},{920,20},
-    {920,80},{920,140},{920,200},{920,260},{920,320},
-    {920,380},{920,440},{920,500},{920,560},{920,620}
+    int red[41][2] = {{890,620},
+    {805,620},{745,620},{685,620},{630,620},{575,620},{520,620},{460,620},{405,620},{350,620},{265,675},{265,550},{265,495},{265,435},{265,380},{265,325},{265,265},{265,205},{265,150},{265,95},{265,20},{350,20},{410,20},{465,20},{520,20},{580,20},{635,20},{690,20},{750,20},{805,20},{890,20},{890,105},{890,160},{890,215},{890,275},{890,330},{890,390},{890,450},{890,505},{890,565},{890,620}};
+
+
+    int green[41][2] = {
+    {920, 620},
+    {835, 620}, {775, 620}, {715, 620}, {660, 620}, {605, 620},
+    {550, 620}, {490, 620}, {435, 620}, {380, 620}, {295, 675},
+    {295, 550}, {295, 495}, {295, 435}, {295, 380}, {295, 325},
+    {295, 265}, {295, 205}, {295, 150}, {295, 95}, {295, 20},
+    {380, 20}, {440, 20}, {495, 20}, {550, 20}, {610, 20},
+    {665, 20}, {720, 20}, {780, 20}, {835, 20}, {920, 20},
+    {920, 105}, {920, 160}, {920, 215}, {920, 275}, {920, 330},
+    {920, 390}, {920, 450}, {920, 505}, {920, 565}, {920, 620}
 };
 
-    static int player1dice;
-    static int player2dice;
-    static int player3dice;
-    static int player4dice;
+
+    int purple[41][2]=  {{890, 650},
+    {805, 650}, {745, 650}, {685, 650}, {630, 650}, {575, 650},
+    {520, 650}, {460, 650}, {405, 650}, {350, 650}, {265, 705},
+    {265, 580}, {265, 525}, {265, 465}, {265, 410}, {265, 355},
+    {265, 295}, {265, 235}, {265, 180}, {265, 125}, {265, 50},
+    {350, 50}, {410, 50}, {465, 50}, {520, 50}, {580, 50},
+    {635, 50}, {690, 50}, {750, 50}, {805, 50}, {890, 50},
+    {890, 135}, {890, 190}, {890, 245}, {890, 305}, {890, 360},
+    {890, 420}, {890, 480}, {890, 535}, {890, 595}, {890, 650}};
+
+    static int dice_red = 0;
+    static int dice_green = 0;
+    static int dice_blue = 0;
+    static int dice_purple = 0;
     
     bool GameOver = false;
 
@@ -57,10 +59,10 @@ int player2vect[40][2] = {
     {
         if (currentState==GameState::State::GameStart) 
         {   
-            PlayerMoney[0].SetAmount(1500);
-            PlayerMoney[1].SetAmount(1500);
-            PlayerMoney[2].SetAmount(1500);
-            PlayerMoney[3].SetAmount(1500);
+            PlayerMoney[0].SetAmount(1000);
+            PlayerMoney[1].SetAmount(1000);
+            PlayerMoney[2].SetAmount(1000);
+            PlayerMoney[3].SetAmount(1000);
             // SDL_Delay(5000);
             // for(int i=0; i<40; i=i+2)
             // {
@@ -75,61 +77,37 @@ int player2vect[40][2] = {
         }
         else if (currentState==GameState::State::Player1Turn) 
         {
-            // PlayerMoney[0].SetAmount(1500);
             SDL_Delay(5000);  
-            int rolledValue1 = RollDice();
-            player1dice+=rolledValue1;
-            gamePieces[0].SetPosition(player1vect[player1dice-1][0], player1vect[player1dice-1][1]);
-            //decidewhattodo(player1vect[a], player1vect[a+1]);
-            // gamePieces[0].MoveRectx(rolledValue * 62);
-            // DisplayCardForBuy("assets/CommunityChest/CommunityChest0.jpg");
+            //int rolledValue1 = RollDice();
+            dice_red+=21;
+            gamePieces[1].SetPosition(green[dice_red][0], green[dice_red][1]);
             gamestate.SetState(GameState::State::Player2Turn);
-            // Piece::MoveRect(85);
         } 
         else if (currentState==GameState::State::Player2Turn) 
         {   
-            // PlayerMoney[1].SubtractAmount(1500);
             SDL_Delay(5000);
-            int rolledValue2 = RollDice();
-            player2dice+=rolledValue2;
-            int b=player2dice*2;
-            gamePieces[1].SetPosition(player2vect[player2dice-1][0], player2vect[player2dice-1][1]);
-            // gamePieces[1].MoveRectx(rolledValue * 62);
-            // DisplayCardForBuy("assets/CommunityChest/CommunityChest1.jpg");
+            //int rolledValue2 = RollDice();
+            dice_green+=29;
+            gamePieces[1].SetPosition(green[dice_green][0], green[dice_green][1]);
             gamestate.SetState(GameState::State::Player3Turn);
         }
         else if (currentState==GameState::State::Player3Turn) 
         {   
-            // PlayerMoney[2].SetAmount(1500);
             SDL_Delay(5000);
-            int rolledValue3 = RollDice();
-            player3dice+=rolledValue3;
-            // int c=player3dice*2;
-            // gamePieces[2].SetPosition(player1vect[c-2], player1vect[c-1]);
-            // int rolledValue = RollDice();
-            // gamePieces[2].MoveRectx(rolledValue * 62);
-            // DisplayCardForBuy("assets/CommunityChest/CommunityChest2.jpg");
+            //int rolledValue3 = RollDice();
+            dice_purple+=39;
+            gamePieces[1].SetPosition(green[dice_purple][0], green[dice_purple][1]);
             gamestate.SetState(GameState::State::Player4Turn);
         }
         else if (currentState==GameState::State::Player4Turn) 
         {   
-            // PlayerMoney[3].SetAmount(1500);
             SDL_Delay(5000);
-            int rolledValue3 = RollDice();
-            player4dice+=rolledValue3;
-            // int d=player4dice*2;
-            // gamePieces[3].SetPosition(player1vect[d-2], player1vect[d-1]);
-            // int rolledValue = RollDice();
-            // gamePieces[3].MoveRecty(rolledValue * 62);
-            // DisplayCardForBuy("assets/CommunityChest/CommunityChest3.jpg");
-            // gamestate.SetState(GameState::State::Player1Turn);
-            gamestate.SetState(GameState::State::Player1Turn);
+            //int rolledValue4 = RollDice();
+            dice_blue+=40;
+            gamePieces[1].SetPosition(green[dice_blue][0], green[dice_blue][1]);
+            gamestate.SetState(GameState::State::GameOver);
+            //GameOver = true;
         }
-        // else if (currentState==GameState::State::GameOver)
-        // {   
-        //     SDL_Delay(3000);
-        //     // return GameOver=false;
-        // }
     return GameOver;
     }
 }
